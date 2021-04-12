3894aced5a32cd0c7b134f63bf342bdb8311ce87
Eugene Govor <eugene.govor@tatemgames.com>
2021-04-12 14:00:26 +0300

    Homework_01 Homework_02

---
+++ Homework_02/Assets/_ROOT/Scripts/UISettingsManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISettingsManager : MonoBehaviour
+{
+    public void ToggleClick()
+    {
+        Debug.Log("Toggle click");
+    }
+
+    public void ButtonClick()
+    {
+        Debug.Log("Button click");
+    }
+}
